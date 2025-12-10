cec56935586fce96b58534dd94f7998925f6ca87
ob1 <ongjjbrandon@gmail.com>
2025-12-10 17:15:14 +0800

    setup

---
+++ docker_container_setup.sh
@@ -0,0 +1,2 @@
+cp -r ./src/openpi/models_pytorch/transformers_replace/* /usr/local/lib/python3.12/dist-packages/transformers/
+cp -r ~/.cache/openpi/openpi-assets/checkpoints/pi05_droid/assets/ /workspace/openpi/torch_pi05_droid/
